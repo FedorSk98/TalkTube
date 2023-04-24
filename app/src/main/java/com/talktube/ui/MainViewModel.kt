0b1531c47fc8f385f6e5e3451126d43f82a9f3b6
DESKTOP-97HDG6E\fedya <fedyaSk98@yandex.ru>
2023-04-24 21:26:33 +0300

    v-0.1.1

---
+++ app/src/main/java/com/talktube/ui/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.talktube.ui
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    fun getVideoDataFromYoutube(){
+
+    }
+
+    fun updateScreen(){
+
+    }
+
+}
---
+++ app/src/main/java/com/talktube/ui/screen/components/AudioPlayerComponent.kt
@@ -0,0 +1,9 @@
+package com.talktube.ui.screen.components
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AudioPlayerComponent(){
+    Text(text = "Hello Huilo!")
+}
